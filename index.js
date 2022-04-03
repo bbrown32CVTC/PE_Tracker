37a1a9d315238a5cb000cf07cd3e76f8e4882b3a
Bruce Brown <bbrown32@student.cvtc.edu>
2022-04-03 15:54:54 -0500

    Add /db-info route with table display.

--- index.js
+++ index.js
@@ -13,8 +13,9 @@ const pool = new Pool({
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .use(express.json())
-  .use(express.urlencoded({ extended: true}))
+  .use(express.urlencoded({ extended: true }))
   .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
   .get('/', async (req, res) => {
     try {
       const client = await pool.connect();
